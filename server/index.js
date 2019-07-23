6f415157fa864d4ad1429de64dd0e8296fd9bf41
Patrick Dureza <pgdureza@gmail.com>
2019-07-23 18:09:01 +0800

    updated graphql server to handle sorting, pagination, date filter, included root data in graphql mock

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const visitors = require('./mocks/visitors.json')
+const sortObjectsArray = require('sort-objects-array')
+const visitorsDataSource = require('./mocks/visitors.json')
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -11,14 +12,50 @@ const typeDefs = gql`
     ip_address: String
   }
 
+  type Root {
+    filters: [String]
+    sort: [String]
+  }
+
   type Query {
-    visitors: [Visitor]
+    visitors(date: String, endDate: String, order: String, sort: String, page: Int): [Visitor]
+    root: Root
   }
 `
 
+const VISITOR_PER_PAGE = 12
+
+const filterByDate = (visitors, date, endDate) => {
+  if (date && endDate) {
+    return visitors.filter(visitor => {
+      const startDateValue = new Date(date)
+      const endDateValue = new Date(endDate)
+      const visitorDateValue = new Date(visitor.date)
+      return startDateValue <= visitorDateValue && visitorDateValue <= endDateValue
+    })
+  }
+  if (date) {
+    return visitors.filter(visitor => visitor.date.indexOf(date) >= 0)
+  }
+  return visitors
+}
+
 const resolvers = {
   Query: {
-    visitors: () => visitors,
+    root: () => {
+      return {
+        sort: ['date', 'device', 'ip'],
+        filters: ['Today', 'Yesterday', 'Last Week', 'This Month'],
+      }
+    },
+    visitors: (_, { date, endDate, page = 1, sort, order = 'asc' }) => {
+      let results = filterByDate(visitorsDataSource, date, endDate)
+      if (sort) {
+        results = sortObjectsArray(results, sort, order)
+      }
+      const lastDisplayed = page * VISITOR_PER_PAGE
+      return results.slice(lastDisplayed - VISITOR_PER_PAGE, lastDisplayed)
+    },
   },
 }
 
